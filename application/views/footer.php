29e0e208e63283f3c9077cab97ce57328eeae942
manuirrazabal <manuel.irrazabalp@gmail.com>
2017-04-01 12:22:14 -0700

    Modification on footer.php linkedin link

--- application/views/footer.php
+++ application/views/footer.php
@@ -12,7 +12,7 @@
     			<p><span> &lt;/&gt; </span> Manuel Irrazabal &copy; 2017 <span> &lt;/&gt; </span></p>
     		</div>
     		<div class="col-sm-2 col-xs-3 col-md-1 col-lg-1">
-    			<a href="www.linkedin.com/in/manuel-irrazabal" target="_blank" style="margin-right: 5px;"><img src="<?php echo base_url().'public/';?>images/linkedin1_icon.png" width="21"></a>
+    			<a href="http://www.linkedin.com/in/manuel-irrazabal" target="_blank" style="margin-right: 5px;"><img src="<?php echo base_url().'public/';?>images/linkedin1_icon.png" width="21"></a>
     			<a href="https://www.instagram.com/manuirrazabal" target="_blank"><img src="<?php echo base_url().'public/';?>images/instagram1_icon.png" width="21"></a>
     		</div>
     	</div>
