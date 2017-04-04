70da10e53073f47b4c6b4bec78abb3d281842fc2
manuirrazabal <manuel.irrazabalp@gmail.com>
2017-04-04 11:30:51 -0700

    Creation 404 error, webpage.

---
+++ application/controllers/My404.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class My404 extends CI_Controller {
+
+	public function __construct() 
+    {
+        parent::__construct(); 
+        $this->layout->setLayout('default');
+    } 
+
+    public function index() 
+    { 
+        $this->output->set_status_header('404'); 
+        $data['content'] = 'error_404'; // View name 
+        $this->layout->view('my404',$data);//loading in my template 
+    } 
+}
---
+++ application/views/my404.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ *
+ * @author	Manuel Irrazabal
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
+ * @copyright	Copyright (c) 2014 - 2015, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+
+
+
+?>
+
+
+<div class="container" style="margin-top: 150px;">
+	<div class="row">
+		<div class="col-md-6 col-md-offset-3 text-center">
+			<br>
+			<!-- <h4>Bob's Camera Site</h4> -->
+			<p><img src="<?php echo base_url(); ?>public/images/404.png" alt="" width="300" ></p>
+
+			<h2><i class="fa fa-exclamation-triangle" style="color:#f4511e;"></i>
+
+			Page not found <small>404 error</small></h2>
+			<p>Well, this is embarrassing.
+
+			<br>
+			</p>
+
+			<p><a href="<?php echo base_url('index'); ?>">Click here</a> to visit our home page</p>
+
+			<p>
+			<a href="http://www.manuirrazabal.com">www.manuirrazabal.com</a></p>
+
+		</div>
+
+	</div>				
+
+</div>
+
+
+
